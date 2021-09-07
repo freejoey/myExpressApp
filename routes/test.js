9eefc60d63e3d5508e8fd9c3c1eadb45ca3e266b
cn8XiaMa <xiao.ma@consultant.lego.com>
2021-09-07 15:38:40 +0800

    refactor

--- routes/test.js
+++ routes/test.js
@@ -1,33 +1,42 @@
 var url = require('url');
 var express = require('express');
 var router = express.Router();
-var TestService = require('../service/TestService')
+var TestService = require('../service/TestService');
+const e = require('express');
+const { error } = require('console');
 var testService = new TestService();
 
-router.get('/get', function(req, res) {
+router.get('/get', function (req, res) {
     console.log(req.query)
-    testService.getData(function cb(data) {
-        res.send(data);    
-    });
+    testService.getData()
+        .then((data) => { res.send(data) })
+        .catch((error) => {
+            console.log(error);
+            res.send({});
+        });
 });
 
-router.get('/add', function(req, res) {
+router.get('/add', function (req, res) {
+    console.log(req.query)
+    params = url.parse(req.url, true).query;
+    testService.addData([params.id, params.name])
+        .then((suc) => res.send({ 'suc': suc }))
+        .catch((error) => {
+            console.log(error);
+            res.send({ "suc": false });
+        });
+});
+
+router.get('/update', function (req, res) {
     console.log(req.query)
 
     params = url.parse(req.url, true).query;
     console.log(params);
-    testService.addData([params.id], function cb(data) {
-        res.send(data);    
-    });
-});
-
-router.get('/update', function(req, res) {
-    console.log(req.query)
-
-    params = url.parse(req.url, true).query;
-    console.log(params);
-    testService.update([params.new_id, params.id], function cb(data) {
-        res.send(data);    
+    testService.update([params.new_id, params.id])
+    .then((suc)=> res.send({'suc': suc}))
+    .catch((error)=>{
+        console.log(error);
+        res.send({"suc": false});
     });
 });
 module.exports = router;
--- service/TestService.js
+++ service/TestService.js
@@ -2,33 +2,45 @@ var connection = require('./db')
 
 class TestService {
 
-    getData(cb) {
-        var data = ''
-        connection.query('SELECT * FROM t', function (error, results, fields) {
-            if (error) throw error;
-            data = JSON.stringify(results);
-            console.log(data);
-            cb(data);
+    getData() {
+        return new Promise((resovle, reject) => {
+            connection.query('SELECT * FROM t', function (error, results, fields) {
+                if (error) reject(error);
+                resovle(JSON.stringify(results));
+            });
         });
     };
 
-    addData(params, cb){
-        var data = '';
-        connection.query('INSERT INTO t(id) VALUES(?)', params, function (error, results, fields) {
-            if (error) throw error;
-            data = JSON.stringify(results);
-            console.log(data);
-            cb(data);
+    addData(params) {
+        return new Promise((resovle, reject) => {
+            connection.query('INSERT INTO t(id, name) VALUES(?, ?)', params, function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                if (results && 1 == results.affectedRows) {
+                    resovle(true);
+                } else {
+                    resovle(false);
+                }
+
+            });
         });
     }
 
-    update(params, cb) {
-        var data = '';
-        connection.query('UPDATE t SET id=? WHERE id=?', params, function (error, results, fields) {
-            if (error) throw error;
-            data = JSON.stringify(results);
-            console.log(data);
-            cb(data);
+    update(params) {
+        return new Promise((resovle, reject) => {
+            connection.query('UPDATE t SET id=? WHERE id=?', params, function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                if (results && 1 == results.affectedRows) {
+                    resovle(true);
+                } else {
+                    resovle(false);
+                }
+            });
         });
     }
 }
